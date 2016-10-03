b53eb68390fe3c09c14e645ac9f16f1db839c07d
Klathmon <gregbenner1@gmail.com>
2016-10-03 18:34:57 -0400

    remove request as a dependency

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
-var request = require('request');
+var http = require('http')
+var https = require('https')
 
 module.exports = {
     report: function(url, msg, cb) {
-        request(url + '?payload=' + encodeURIComponent(msg), function (error, response, body) {
-		cb('success');
+      var module
+      // If the url begins with https then we need to use the https module, otherwise use http
+      if (url.match(/^https/i)) {
+        module = https
+      } else {
+        module = http
+      }
+        module.get(url + '?payload=' + encodeURIComponent(msg), function (error, response, body) {
+            cb('success');
         });
     }
 };
